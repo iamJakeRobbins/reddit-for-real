d68bee95d8de83b8efd246fa21637398605f1567
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2017-11-06 13:25:36 -0700

    we added require and app.use for the hero route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-// var
+var hero = require('./routes/hero');
 
 var app = express();
 
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/hero', hero);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
